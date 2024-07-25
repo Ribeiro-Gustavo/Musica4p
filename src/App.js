2d44c971cc79cd68f18dc6626a59e81dbbb1dcb9
Ribeiro-Gustavo <engs-gustavofranca@camporeal.edu.br>
2024-07-25 20:28:12 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Artista from './components/Artista';
+
+function App(){
+  return(
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/artista">Artista</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/artista" element={<Artista/>}/>
+          <Route path="/" element={<Home/>}/> 
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Artista.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Artista(){
+    return <h2>Página do Artista</h2>
+    <h1>Banana </h1>;
+}
+
+export default Artista;
